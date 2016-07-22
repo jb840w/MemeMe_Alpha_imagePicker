2030454eddd12bc3efbd6b58aa22caafa4ea0a72
Jeremy Benson <jeremy@ashleybensonfitness.com>
2016-07-22 15:58:07 -0500

    initial commit

--- MemeMe_Alpha_imagePicker/ViewController.swift
+++ MemeMe_Alpha_imagePicker/ViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var actionButton: UIBarButtonItem!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Action to choose the camera or album
+    @IBAction func pickAnImage(sender: AnyObject) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        // if statement, which picker was chosen
+        if sender.tag == 0 {
+            pickerController.sourceType = .Camera
+            self.presentViewController(pickerController, animated: true, completion: nil)
+        } else if sender.tag == 1 {
+            pickerController.sourceType = .PhotoLibrary
+            self.presentViewController(pickerController, animated: true, completion: nil)
+        }
+        
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePickerView.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
     }
 
 
