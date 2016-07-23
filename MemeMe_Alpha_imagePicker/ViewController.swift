62737e58906087e12e5055f8758be617ed6efa17
Jeremy Benson <jeremy@ashleybensonfitness.com>
2016-07-23 17:11:24 -0500

    added share functionality to action button

--- MemeMe_Alpha_imagePicker/ViewController.swift
+++ MemeMe_Alpha_imagePicker/ViewController.swift
@@ -19,12 +19,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
 
     // Action to choose the camera or album
     @IBAction func pickAnImage(sender: AnyObject) {
+        
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        
         // if statement, which picker was chosen
         if sender.tag == 0 {
             pickerController.sourceType = .Camera
@@ -37,9 +40,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
+    @IBAction func activityShareAction(sender: UIBarButtonItem) {
+        let image = imagePickerView.image!
+        let itemsToShare = [image]
+        let activityViewController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        activityViewController.excludedActivityTypes = []
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+    }
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         imagePickerView.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
     }
+    
 
 
 }
